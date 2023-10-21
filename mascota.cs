e7350ed8d270762ed04295966dcb3bd97a1d308b
Osler Daniel Méndez Godoy <147261056+osler12@users.noreply.github.com>
2023-10-20 21:00:06 -0700

    Update mascota.cs
    Treabajo listo

--- mascota.cs
+++ mascota.cs
@@ -1,14 +1,33 @@
 using System;
 
-public class Mascota
+namespace ClaseMascota
 {
-     
-}
-
-class Program
-{
-    static void Main(string[] args)
+    class Mascota
     {
-        Console.WriteLine("¡Hola, mundo!");
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public int Edad { get; set; }
+
+        public Mascota(string nombre, string tipo, int edad)
+        {
+            Nombre = nombre;
+            Tipo = tipo;
+            Edad = edad;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mascota mascota1 = new Mascota("Haru", "Perro", 2);
+            Mascota mascota2 = new Mascota("Pelusa", "Gato", 4);
+            Mascota mascota3 = new Mascota("mart", "Pez", 1);
+
+            Console.WriteLine("Mascota 1 - Nombre:" + mascota1.Nombre+  "  Tipo: " + mascota1.Tipo + " Edad: " + mascota1.Edad + " años");
+            Console.WriteLine("Mascota 2 - Nombre:" + mascota2.Nombre + " Tipo: " + mascota2.Tipo + " Edad: " + mascota2.Edad + " años");
+            Console.WriteLine("Mascota 3 - Nombre:" + mascota3.Nombre + "  Tipo: " + mascota3.Tipo + " Edad: " + mascota3.Edad + " años");
+            Console.ReadLine();
+        }
     }
 }
