18d1ad7b57924ec8118f267b6477fccecfab735d
Osler Daniel Méndez Godoy <147261056+osler12@users.noreply.github.com>
2023-10-20 20:32:30 -0700

    Update empleado.cs
    Entrega

--- empleado.cs
+++ empleado.cs
@@ -2,14 +2,12 @@ using System;
 
 public class Empleado
 {
-   
     public string Nombre { get; set; }
-    public double Salario { get; set; }
+    public decimal Salario { get; set; }
 
-    
-    public double CalcularSalarioAnual()
+    public decimal CalculaSalarioAnual()
     {
-        
+        return Salario * 12;
     }
 }
 
@@ -17,6 +15,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("¡Hola, mundo!");
+        Empleado empleado = new Empleado();
+
+        Console.Write("Introduce el nombre del empleado: ");
+        empleado.Nombre = Console.ReadLine();
+
+        Console.Write("Introduce el salario mensual del empleado: ");
+        empleado.Salario = decimal.Parse(Console.ReadLine());
+
+        decimal salarioAnual = empleado.CalculaSalarioAnual();
+
+        Console.WriteLine("El salario anual de {0} es: {1}", empleado.Nombre, salarioAnual);
+        Console.ReadLine();
     }
 }
